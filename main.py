859254adba3d9dd6702f3219615a3ef7daf99a97
Andriamanitra <mikko77792@gmail.com>
2022-06-13 22:47:23 +0300

    move eval code to separate file

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import ast
-import copy
-import contextlib
-import io
-import sys
-from dataclasses import dataclass
+from evals import EvalResult
+
 import tkinter
 from tkinter import BOTH
 from tkinter import ttk
@@ -126,49 +122,6 @@ class InputCell(tkinter.Frame):
 
     def focus(self):
         self.text_input.focus()
-
-
-@dataclass
-class EvalResult:
-    stdout: str
-    stderr: str
-    representation: str
-    local_vars: dict
-
-    @classmethod
-    def from_code(cls, code: str, local_vars_before: dict):
-        local_vars = dict()
-        for name, value in local_vars_before.items():
-            try:
-                copied = copy.deepcopy(value)
-            # some things like modules cannot be deepcopied
-            # (hopefully this exception handling doesn't break too many things)
-            except TypeError:
-                copied = value
-            local_vars[name] = copied
-
-        result = None
-        my_stdout = io.StringIO()
-        my_stderr = io.StringIO()
-        with (
-                contextlib.redirect_stdout(my_stdout),
-                contextlib.redirect_stderr(my_stderr),
-        ):
-            try:
-                body = ast.parse(code).body
-                if len(body) == 1 and isinstance(body[0], ast.Expr):
-                    result = repr(eval(code, None, local_vars))
-                else:
-                    exec(code, None, local_vars)
-            except Exception as exc:
-                print(exc, file=sys.stderr)
-
-        return EvalResult(
-            my_stdout.getvalue(),
-            my_stderr.getvalue(),
-            result,
-            local_vars
-        )
 
 
 def main():
